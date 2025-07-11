762e3ead02aa16a54424691f62788e0c26fed0d2
ganeshmoka <ganeshmoka1999@gmail.com>
2025-07-11 22:55:41 +0530

    def call(map configmap)

---
+++ vars/nodeJSEKSpipeline.groovy
@@ -0,0 +1,112 @@
+def call(Map configMap){
+    pipeline {
+        agent {
+            label 'AGENT-1'
+        }
+    
+        environment {
+            appVersion = '' //global variable
+            environment = 'dev'
+            project = configMap.get("project")
+            component = configMap.get("component")
+        }
+
+        stages {
+            stage ('read package json') { //Pipeline Utility Steps plugin.
+                steps {
+                    script {
+                        def packageJson = readJSON file: 'package.json'
+                        appVersion = packageJson.version
+                        echo "app version: ${appVersion}"
+                    }
+                }
+            }
+        /* stage ('sonar Analysis'){
+                environment {
+                    SCANNER_HOME = tool 'sonar-6.0' // the name used while setup of scanner 
+                }
+                steps{
+                    withSonarQubeEnv('Sonar') {
+                        sh '${SCANNER_HOME}/bin/sonar-scanner' 
+                
+                    }
+                    // sonar-scanner check for files sonar-project.properties     
+                }
+            }
+            stage("Quality Gate") {
+                steps {
+                    timeout(time: 5, unit: 'MINUTES') {
+                        // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
+                        // true = set pipeline to UNSTABLE, false = don't
+                        waitForQualityGate abortPipeline: true
+                    }
+                }
+            } */
+
+            stage ('docker image build') {
+                steps {
+                    withAWS(region: 'us-east-1', credentials: 'aws-cred') {
+                        sh """
+                        aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 905418383993.dkr.ecr.us-east-1.amazonaws.com
+                        docker build -t expense/backend:${appVersion} .
+                        docker images
+                        docker tag expense/backend:${appVersion} 905418383993.dkr.ecr.us-east-1.amazonaws.com/expense/backend:${appVersion}
+                        docker push 905418383993.dkr.ecr.us-east-1.amazonaws.com/expense/backend:${appVersion}
+                        """
+                    } 
+                }
+
+            }
+            /*
+            stage ('deploy') {
+                steps {
+                    withAWS(region: 'us-east-1', credentials: 'aws-cred') {
+                        sh """
+                        aws eks update-kubeconfig --region us-east-1 --name ${project}-${environment}
+                        cd helm
+                        sed -i 's/IMAGE_VERSION/${appVersion}/g' values-${environment}.yaml
+                        helm upgrade --install ${component} -n ${project} -f values-${environment}.yaml .
+                        """
+                    }
+                    // i - replace in IMAGE_VERSION value with $appversion value , target file is values-dev.yaml          
+                }
+            }
+
+            stage ('trigger frontend') {
+                steps {
+                    echo 'triggering frontend'
+                    build job: "frontend", wait: true
+                }
+                
+            }
+            */
+            stage ('deploy-cd') {
+                steps {
+                    echo 'triggering backend cd'
+                    build job: "backend-cd",parameters: [string(name: version, values: "$appVersion"),string(name: )], wait: true
+                }
+                
+            }
+
+
+        }
+        
+        post { 
+            always { 
+                echo 'Deleting workspace artifacts...!'  // this always executes if pipeline fail or success.
+                deleteDir()
+
+            }
+
+            failure {
+                echo 'pipeline is fail!'  // this will executes if pipeline fail
+            }
+
+            success {
+                echo 'pipeline is success!'  // this will executes if pipeline success
+            }
+
+        }
+    }
+
+}
